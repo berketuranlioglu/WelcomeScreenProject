2a7d8650bfaf4ceb4fcca5cbf205478d03d617bf
Berke Turanlioglu <berke.turanlioglu@gmail.com>
2023-08-21 21:41:28 +0300

    Initial Commit

---
+++ WelcomeScreenProject/WelcomeScreenProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  WelcomeScreenProjectApp.swift
+//  WelcomeScreenProject
+//
+//  Created by Berke Turanlıoğlu on 21.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct WelcomeScreenProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
